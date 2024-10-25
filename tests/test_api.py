5a79a42e6418d2450f82b07abce83e0846a03273
Laís Urano <lais.urano@gmail.com>
2024-10-25 18:44:57 -0300

    testes de produtos e usuários

--- tests/test_api.py
+++ tests/test_api.py
@@ -13,3 +13,34 @@ def test_home():
     }
 
 
+def test_criar_produto():
+    response = client.post(
+        "/produtos/",
+        json={
+            "nome": "Produto A",
+            "categoria": "Categoria 1",
+            "tags": ["tag1", "tag2"],
+        },
+    )
+    assert response.status_code == 200
+    assert response.json()["nome"] == "Produto A"
+
+
+def test_listar_produtos():
+    response = client.get("/produtos/")
+    assert response.status_code == 200
+    assert len(response.json()) == 1
+
+
+def test_criar_usuario():
+    response = client.post("/usuarios/", params={"nome": "Usuário Teste"})
+    assert response.status_code == 200
+    usuario_data = response.json()
+    assert usuario_data["id"] == 1
+    assert usuario_data["nome"] == "Usuário Teste"
+
+
+def test_listar_usuarios():
+    response = client.get("/usuarios/")
+    assert response.status_code == 200
+    assert len(response.json()) == 1
