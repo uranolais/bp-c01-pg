9d513a1f958704494f9446bc707cedb262ee7dc1
Laís Urano <lais.urano@gmail.com>
2024-10-25 18:34:22 -0300

    teste inicial

---
+++ tests/test_api.py
@@ -0,0 +1,15 @@
+import pytest
+from fastapi.testclient import TestClient
+from app.main import app
+
+client = TestClient(app)
+
+
+def test_home():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {
+        "mensagem": "Bem-vindo à API de Recomendação de Produtos"
+    }
+
+
